c1f6597e1e724dbd559aaa41a5fab45478a8b528
saifgehlot07 <gehlotsaif07@gmail.com>
2023-01-01 11:17:25 +0530

    Ui finished

---
+++ script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// Selection
+const playNow = document.querySelector(".play-now");
+const musicImg = document.querySelector(".music-img");
+const musicName = document.querySelector(".music-name");
+const musicArtist = document.querySelector(".artist-name");
+const musicAudio = document.querySelector(".audio");
+
+// All Music Data
+const allMusic = [
+  {
+    musicImage: "./images/sg-1.png",
+    musicName: "Manike",
+    musicArtist: "Jubin Nautiyal",
+    src: "./music/audio-1.mp3",
+  },
+  {
+    musicImage: "./images/sg-2.png",
+    musicName: "Maine Royaan",
+    musicArtist: "Tanveer Evan",
+    src: "./music/audio-2.mp3",
+  },
+  {
+    musicImage: "./images/sg-2.png",
+    musicName: "Sugar & Brownies",
+    musicArtist: "Dharia",
+    src: "./music/audio-3.mp3",
+  },
+];
